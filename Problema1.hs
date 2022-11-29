dd604d3729c77a048225c8f96ee0cb9cad11ef70
Rui Oliveira <rpoliveira02@gmail.com>
2022-11-29 19:56:56 +0000

    1st exercise

---
+++ Problema1.hs
@@ -0,0 +1,13 @@
+module Problema1 where
+
+import Cp
+import Nat
+
+k2 a b c ((x, y), z) = a * y + b * x + c * z
+
+f _ _ _ 0 = 0
+f _ _ _ 1 = 1
+f _ _ _ 2 = 1
+f a b c n = a * (f a b c (n - 1)) + b * (f a b c (n - 2)) + c * (f a b c (n - 3))
+
+f1 a b c = p2 . for (split (split (k2 a b c) p2) (p2 . p1)) ((1,1), 0)
